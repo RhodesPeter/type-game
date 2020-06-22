6e60c71dd0ef732743889e65190faab1bb6d64e2
Peter Rhodes <rhodespeter@hotmail.co.uk>
2020-06-22 15:13:03 +0100

    Add nunjucks

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,27 @@
-var express = require('express');
-var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var sockets = require('./sockets');
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const sockets = require('./sockets');
+const nunjucks = require('nunjucks');
 
 const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/templates/index.html');
+nunjucks.configure(path.join(__dirname, 'views'), {
+  autoescape: true,
+  express: app
 });
 
 app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.render('pages/index.html');
+});
+
+app.get('/play', function (req, res) {
+  res.render('pages/play.html');
+});
 
 http.listen(port, function () {
   console.log(`listening on port ${port}`);
